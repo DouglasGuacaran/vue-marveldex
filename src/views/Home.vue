a8a0e1f525e60a3ee87597c43a77d6d16da51a25
DouglasGuacaran <douglasguacaran@gmail.com>
2023-02-08 13:35:06 -0300

    Agrego imagen de bienvenida

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
-  <b-row class="home">
-    <b-col>
-      <h1 class="justify-content-center">Bienvenidos a mi Marvel Dex</h1>
-      <h2>Podrás hacer scroll y conocer los datos de algunos de los personajes emblemáticos del Universo Marvel</h2>
-    </b-col>
-  </b-row>
+  <b-container>
+    <b-row class="mt-4" >
+      <b-col xs="12" align="center" class="mx-auto">
+        <h1>Bienvenidos a mi página sobre Marvel</h1>
+        <b-img class="image" :src="require('../assets/Images/The_Marvel_Universe.png')"></b-img>
+        <h2>Donde podrás conocer los datos de algunos de los personajes emblemáticos del Universo Marvel</h2>
+        <b-button class="mt-3" variant="warning" @click="$router.push('/marveldex')">Adelante</b-button>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
 export default {
   name: 'Home',
 }
 </script>
+
+<style>
+  .image{
+    width: 90%;
+  }
+
+  @media screen and (max-width:690px) {
+    .image{
+      width: 85%;
+    }
+    
+  }
+</style>
+
